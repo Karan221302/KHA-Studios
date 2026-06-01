f1491b2cd6b36b81e4cc67b7e6455f13cae9e841
Karan <karandharmdhikari@gmail.com>
2026-06-01 13:24:33 +0530

    Add google serach console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "KHA STUDIOS | Architecture & Interior Design",
   description:
-    "KHA Studios provides architectural design, interior design, planning and 3D visualization servies.",
+    "KHA Studios provides architectural design, interior design, planning and 3D visualization services.",
     verification:{
       google: "uIYKlljoFmBqCPZ9qHBdyn7EWl5Dq4uwvoUnPhk5RpU"
     }
